c1feba7625c4fe9247cd39a1abed9e74adfdb6bf
aqwuarvitha <aqwuarvitha@gmail.com>
2019-10-09 10:08:31 +0700

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,20 +12,6 @@
 					<li class="active">Dashboard </li>
 				</ol>
 
-				<div class="page@extends('backend/layouts.template')
-
-@section('content')
-<div class="main-content">
-	<div class="container">
-		<!-- start: Breadcrumb and Title -->
-		<div class="row">
-			<div class="col-sm-12">
-
-				<ol class="breadcrumb">
-					<li><i class="clip-home-3"></i><a href="{{ url('/') }}"> Home</a></li>
-					<li class="active">Dashboard </li>
-				</ol>
-
 				<div class="page-header">
 					<h1>Dashboard <small>overview &amp; stats </small></h1>
 				</div>
